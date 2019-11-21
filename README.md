b92016c656de6a67fe6732403df3b32d13544c97
halilemincaliskan <44180876+halilemincaliskan@users.noreply.github.com>
2019-11-21 16:12:22 +0300

    Cpp 8. Ders Örnek
    Derste yapılması gereken örnek

---
+++ README.md
@@ -0,0 +1,2 @@
+# cppDers8
+ İstanbul Üniversitesi Bilgisayar Mühendisliği Introducing to Algorithm 8. Dersinde yapılması gereken örnek.
---
+++ cppDers8/cppDers8.cpp
@@ -0,0 +1,42 @@
+﻿// Gerekli kütüphaneler vs. eklendi.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/* Gerekli tanımlamaları yaptım.
+		n -> Sayısal değerin kaça kadar gitmesi gerektiği.
+		row -> Satır sayısı.
+		number -> Döngü içerisindeki ekrana bastırılan değer.
+	*/
+	int n, row;
+	int number = 1;
+
+	// Kullanıcıdan alınması gereken değerleri aldım.
+	cout << "Bir n değeri giriniz :" << endl;
+	cin >> n;
+	cout << "Bir row değeri giriniz :";
+	cin >> row;
+
+	// Döngülerin ilkinin amacı satırı ikincisi ise sütunu dönmektir.
+	for (int i = 1; i <= row; i++) {
+		for (int j = 0; j < row; j++) {
+			if (j >= (row - i) && number <= n) {
+				cout << number;
+				number++;
+			}
+			else if (number > n) {
+				cout << "*";
+			}
+			else {
+				cout << " ";
+			}
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
+/* Çıktıda normalde amacım sağ tarafa dayalı şekilde bir çıktı almak ancak
+   9'dan sonra basamak sayısı 2 olduğu için iki sayılık yer kaplıyor ondan
+   dolayı iki basamaklı sayıların geldiği sütunlar sağ doğru kaymaya başlıyor.*/
